2dbef72f8863127a2443915d24dbe9ae1943de00
antsfamily <zhiliu.mind@gmail.com>
2021-01-10 10:23:03 +0800

    add device

---
+++ tests/device/th_conv_device.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+import time
+
+
+
+device = 'cuda:0'
+device = 'cuda:1'
+
+niters = 1000
+
+print("Torch version: ", torch.__version__)
+print("Torch CUDA version: ", torch.version.cuda)
+print("CUDNN Version: ", torch.backends.cudnn.version())
+print(torch.cuda.get_device_name(int(device[-1])))
+
+def profile(model, x, benchmark, deterministic, nb_iters):
+    torch.backends.cudnn.benchmark = benchmark
+    torch.backends.cudnn.deterministic = deterministic
+
+    # warmup
+    for _ in range(10):
+        out = model(x)
+
+    torch.cuda.synchronize()
+    t0 = time.time()
+    for _ in range(nb_iters):
+        out = model(x)
+    torch.cuda.synchronize()
+    t1 = time.time()
+
+    return (t1 - t0) / nb_iters
+
+
+model1 = nn.Sequential(
+    nn.Conv1d(24, 256, kernel_size=(12,), stride=(6,), groups=4),
+    nn.ReLU(),
+    nn.Conv1d(256, 256, kernel_size=(6,), stride=(3,), padding=(2,), groups=4),
+    nn.ReLU(),
+    nn.Conv1d(256, 256, kernel_size=(3,), stride=(1,), padding=(1,), groups=4),
+    nn.ReLU(),
+)
+
+model1.to(device=device)
+
+x = torch.randn(64, 24, 224, device=device)
+
+time0 = profile(model1, x, benchmark=False, deterministic=False, nb_iters=niters)
+print('Conv1d model, benchmark=False, deterministic=False, {:.3f}ms/iter'.format(time0*1000))
+time1 = profile(model1, x, benchmark=True, deterministic=False, nb_iters=niters)
+print('Conv1d model, benchmark=True, deterministic=False, {:.3f}ms/iter'.format(time1*1000))
+time2 = profile(model1, x, benchmark=False, deterministic=True, nb_iters=niters)
+print('Conv1d model, benchmark=False, deterministic=True, {:.3f}ms/iter'.format(time2*1000))
+time3 = profile(model1, x, benchmark=True, deterministic=True, nb_iters=niters)
+print('Conv1d model, benchmark=True, deterministic=True, {:.3f}ms/iter'.format(time3*1000))
+
+model2 = nn.Sequential(
+    nn.Conv2d(8, 32, kernel_size=(8, 8), stride=(4, 4)),
+    nn.ReLU(),
+    nn.Conv2d(32, 64, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+    nn.ReLU()
+)
+model2.to(device=device)
+
+x = torch.randn(64, 8, 224, 224, device=device)
+time0 = profile(model2, x, benchmark=False, deterministic=False, nb_iters=niters)
+print('Conv2d model, benchmark=False, deterministic=False, {:.3f}ms/iter'.format(time0*1000))
+time1 = profile(model2, x, benchmark=True, deterministic=False, nb_iters=niters)
+print('Conv2d model, benchmark=True, deterministic=False, {:.3f}ms/iter'.format(time1*1000))
+time2 = profile(model2, x, benchmark=False, deterministic=True, nb_iters=niters)
+print('Conv2d model, benchmark=False, deterministic=True, {:.3f}ms/iter'.format(time2*1000))
+time3 = profile(model2, x, benchmark=True, deterministic=True, nb_iters=niters)
+print('Conv2d model, benchmark=True, deterministic=True, {:.3f}ms/iter'.format(time3*1000))
+
+model3 = nn.Sequential(
+    nn.Conv2d(8, 32, kernel_size=(8, 1), stride=(4, 1)),
+    nn.ReLU(),
+    nn.Conv2d(32, 64, kernel_size=(4, 1), stride=(2, 1), padding=(1, 1)),
+    nn.ReLU(),
+    nn.Conv2d(64, 64, kernel_size=(3, 1), stride=(1, 1), padding=(1, 1)),
+    nn.ReLU()
+)
+model3.to(device=device)
+
+x = torch.randn(64, 8, 224, 224, device=device)
+time0 = profile(model3, x, benchmark=False, deterministic=False, nb_iters=niters)
+print('Conv2d1 model, benchmark=False, deterministic=False, {:.3f}ms/iter'.format(time0*1000))
+time1 = profile(model3, x, benchmark=True, deterministic=False, nb_iters=niters)
+print('Conv2d1 model, benchmark=True, deterministic=False, {:.3f}ms/iter'.format(time1*1000))
+time2 = profile(model3, x, benchmark=False, deterministic=True, nb_iters=niters)
+print('Conv2d1 model, benchmark=False, deterministic=True, {:.3f}ms/iter'.format(time2*1000))
+time3 = profile(model3, x, benchmark=True, deterministic=True, nb_iters=niters)
+print('Conv2d1 model, benchmark=True, deterministic=True, {:.3f}ms/iter'.format(time3*1000))
+
--- torchlib/dsp/ffts.py
+++ torchlib/dsp/ffts.py
@@ -7,6 +7,112 @@
 
 import numpy as np
 import torch as th
+import torch.fft as thfft
+
+
+def freq(fs, n, norm=False, shift=False, dtype=th.float32):
+    r"""Return the sample frequencies
+
+    Return the sample frequencies.
+
+    Given a window length `n` and a sample rate `fs`, if shift is ``True``::
+
+      f = [-n/2, ..., n/2] / (d*n)
+
+    Given a window length `n` and a sample rate `fs`, if shift is ``False``::
+
+      f = [0, 1, ..., n] / (d*n)
+
+    If :attr:`norm` is ``True``, :math:`d = 1`, else :math:`d = 1/f_s`.
+
+    Parameters
+    ----------
+    fs : {float}
+        Sampling rate.
+    n : {integer}
+        Number of samples.
+    norm : {bool}
+        Normalize the frequencies.
+    shift : {bool}
+        Does shift the zero frequency to center.
+    dtype : {torch tensor type}
+        Data type, default is ``th.float32``.
+
+    Returns
+    -------
+    torch 1d-tensor
+        Frequency array with size :math:`n×1`.
+    """
+
+    d = 1. / fs
+
+    if shift:
+        f = np.linspace(-n / 2., n / 2., n, endpoint=True)
+    else:
+        f = np.linspace(0, n, n, endpoint=True)
+
+    if norm:
+        return th.tensor(f / n, dtype=dtype)
+    else:
+        return th.tensor(f / (d * n), dtype=dtype)
+
+
+def fftfreq(fs, n, norm=False, shift=False, dtype=th.float32):
+    r"""Return the Discrete Fourier Transform sample frequencies
+
+    Return the Discrete Fourier Transform sample frequencies.
+
+    Given a window length `n` and a sample rate `fs`, if shift is ``True``::
+
+      f = [-n/2, ..., -1,     0, 1, ...,   n/2-1] / (d*n)   if n is even
+      f = [-(n-1)/2, ..., -1, 0, 1, ..., (n-1)/2] / (d*n)   if n is odd
+
+    Given a window length `n` and a sample rate `fs`, if shift is ``False``::
+
+      f = [0, 1, ...,   n/2-1,     -n/2, ..., -1] / (d*n)   if n is even
+      f = [0, 1, ..., (n-1)/2, -(n-1)/2, ..., -1] / (d*n)   if n is odd
+
+    where :math:`d = 1/f_s`, if :attr:`norm` is ``True``, :math:`d = 1`, else :math:`d = 1/f_s`.
+
+    Parameters
+    ----------
+    fs : {float}
+        Sampling rate.
+    n : {integer}
+        Number of samples.
+    norm : {bool}
+        Normalize the frequencies.
+    shift : {bool}
+        Does shift the zero frequency to center.
+    dtype : {torch tensor type}
+        Data type, default is ``th.float32``.
+
+    Returns
+    -------
+    torch 1d-array
+        Frequency array with size :math:`n×1`.
+    """
+    d = 1. / fs
+    if n % 2 == 0:
+        N = n
+        N1 = int(n / 2.)
+        N2 = int(n / 2.)
+        endpoint = False
+    else:
+        N = n - 1
+        N1 = int((n + 1) / 2.)
+        N2 = int((n - 1) / 2.)
+        endpoint = True
+
+    if shift:
+        f = np.linspace(-N / 2., N / 2., n, endpoint=endpoint)
+    else:
+        f = np.hstack((np.linspace(0, N / 2., N1, endpoint=endpoint),
+                       np.linspace(-N / 2., 0, N2, endpoint=False)))
+    if norm:
+        return th.tensor(f / n, dtype=dtype)
+    else:
+        return th.tensor(f / (d * n), dtype=dtype)
 
 
 def fftshift(x, axis=None):
@@ -33,22 +139,24 @@ def fftshift(x, axis=None):
 
     Examples
     --------
+    ::
+
         import numpy as np
-        import torchlib as tl
+        import tsar as ts
         import torch as th
 
         x = [1, 2, 3, 4, 5, 6]
         y = np.fft.fftshift(x)
         print(y)
         x = th.tensor(x)
-        y = tl.fftshift(x)
+        y = ts.fftshift(x)
         print(y)
 
         x = [1, 2, 3, 4, 5, 6, 7]
         y = np.fft.fftshift(x)
         print(y)
         x = th.tensor(x)
-        y = tl.fftshift(x)
+        y = ts.fftshift(x)
         print(y)
 
         axis = (0, 1)  # axis = 0, axis = 1
@@ -56,7 +164,7 @@ def fftshift(x, axis=None):
         y = np.fft.fftshift(x, axis)
         print(y)
         x = th.tensor(x)
-        y = tl.fftshift(x, axis)
+        y = ts.fftshift(x, axis)
         print(y)
 
 
@@ -64,7 +172,7 @@ def fftshift(x, axis=None):
         y = np.fft.fftshift(x, axis)
         print(y)
         x = th.tensor(x)
-        y = tl.fftshift(x, axis)
+        y = ts.fftshift(x, axis)
         print(y)
 
     """
@@ -104,22 +212,24 @@ def ifftshift(x, axis=None):
 
     Examples
     --------
+    ::
+
         import numpy as np
-        import torchlib as tl
+        import tsar as ts
         import torch as th
 
         x = [1, 2, 3, 4, 5, 6]
         y = np.fft.fftshift(x)
         print(y)
         x = th.tensor(x)
-        y = tl.fftshift(x)
+        y = ts.fftshift(x)
         print(y)
 
         x = [1, 2, 3, 4, 5, 6, 7]
         y = np.fft.fftshift(x)
         print(y)
         x = th.tensor(x)
-        y = tl.fftshift(x)
+        y = ts.fftshift(x)
         print(y)
 
         axis = (0, 1)  # axis = 0, axis = 1
@@ -127,7 +237,7 @@ def ifftshift(x, axis=None):
         y = np.fft.fftshift(x, axis)
         print(y)
         x = th.tensor(x)
-        y = tl.fftshift(x, axis)
+        y = ts.fftshift(x, axis)
         print(y)
 
 
@@ -135,7 +245,7 @@ def ifftshift(x, axis=None):
         y = np.fft.fftshift(x, axis)
         print(y)
         x = th.tensor(x)
-        y = tl.fftshift(x, axis)
+        y = ts.fftshift(x, axis)
         print(y)
 
     """
@@ -158,7 +268,7 @@ def padfft(X, nfft=None, axis=0, shift=False):
 
     Parameters
     ----------
-    X : {torch.ndarray}
+    X : {torch tensor}
         Data to be padded.
     nfft : {number or None}
         Padding size.
@@ -166,6 +276,11 @@ def padfft(X, nfft=None, axis=0, shift=False):
         Padding dimension. (the default is 0)
     shift : {bool}, optional
         Whether to shift the frequency (the default is False)
+
+    Returns
+    -------
+    y : {torch tensor}
+        The padded tensor.
     """
 
     if axis is None:
@@ -195,85 +310,26 @@ def padfft(X, nfft=None, axis=0, shift=False):
     return X
 
 
-def fftfreq(fs, n, norm=False, shift=False):
-    r"""Return the Discrete Fourier Transform sample frequencies
+def fft(x, n=None, axis=0, norm="backward", shift=False):
+    """FFT in tsar
 
-    Return the Discrete Fourier Transform sample frequencies.
-
-    The returned float array `f` contains the frequency bin centers in cycles
-    per unit of the sample spacing (with zero at the start).  For instance, if
-    the sample spacing is in seconds, then the frequency unit is cycles/second.
-
-    Given a window length `n` and a sample spacing `d`, if shift is ``True`` and ``norm`` is True::
-
-      f = [-n/2, ..., -1,     0, 1, ...,   n/2-1] / (d*n)   if n is even
-      f = [-(n-1)/2, ..., -1, 0, 1, ..., (n-1)/2] / (d*n)   if n is odd
-
-    Given a window length `n` and a sample spacing `d`, if shift is ``False``::
-
-      f = [0, 1, ...,   n/2-1,     -n/2, ..., -1] / (d*n)   if n is even
-      f = [0, 1, ..., (n-1)/2, -(n-1)/2, ..., -1] / (d*n)   if n is odd
-
-    where :math:`d = 1/f_s`.
-
-    Parameters
-    ----------
-    fs : {float}
-        Sampling rate.
-    n : {integer}
-        Number of samples.
-    norm : {bool}
-        Normalize the frequencies.
-    shift : {bool}
-        Does shift the zero frequency to center (Default is ``False``).
-
-    Returns
-    -------
-    torch tensor
-        frequency array with size :math:`n×1`.
-    """
-    d = 1. / fs
-    if n % 2 == 0:
-        N = n
-        N1 = int(n / 2.)
-        N2 = int(n / 2.)
-        endpoint = False
-    else:
-        N = n - 1
-        N1 = int((n + 1) / 2.)
-        N2 = int((n - 1) / 2.)
-        endpoint = True
-
-    if shift:
-        f = np.linspace(-N / 2., N / 2., n, endpoint=endpoint)
-    else:
-        f = np.hstack((np.linspace(0, N / 2., N1, endpoint=endpoint),
-                       np.linspace(-N / 2., 0, N2, endpoint=False)))
-    if norm:
-        return th.tensor(f / n, dtype=th.float32)
-    else:
-        return th.tensor(f / (d * n), dtype=th.float32)
-
-
-def fft(x, nfft=None, axis=0, norm=False, shift=False):
-    """FFT in torchlib
-
-    IFFT in torchlib, since ifft in torch only supports complex-complex transformation,
-    for real ifft, we insert imaginary part with zeros (torch.stack((x,torch.zeros_like(x), dim=-1))),
-    also you can use torch's rifft.
+    FFT in tsar.
 
     Parameters
     ----------
     x : {torch array}
-        both complex and real representation are supported. Since torch does not
-        support complex array, when :attr:`x` is complex, we will change the representation
-        in real formation(last dimension is 2, real, imag), after FFT, it will be change back.
-    nfft : {integer}, optional
+        complex representation is supported. Since torch1.7 and above support complex array,
+        when :attr:`x` is in real-representation formation(last dimension is 2, real, imag),
+        we will change the representation in complex formation, after FFT, it will be change back.
+    n : {integer}, optional
         number of fft points (the default is None --> equals to signal dimension)
     axis : {number}, optional
         axis of fft (the default is 0, which the first dimension)
-    norm : {bool}, optional
-        normalization (the default is False)
+    norm : {None or str}, optional
+        Normalization mode. For the forward transform (fft()), these correspond to:
+        - "forward" - normalize by ``1/n``
+        - "backward" - no normalization (default)
+        - "ortho" - normalize by ``1/sqrt(n)`` (making the FFT orthonormal)
     shift : {bool}, optional
         shift the zero frequency to center (the default is False)
 
@@ -288,37 +344,38 @@ def fft(x, nfft=None, axis=0, norm=False, shift=False):
         nfft is small than signal dimension.
     """
 
-    if th.is_complex(x):
-        cplxflag = True
-        x = th.view_as_real(x)
+    if norm is None:
+        norm = 'backward'
+
+    if (x.size(-1) == 2) and (not th.is_complex(x)):
+        realflag = True
+        x = th.view_as_complex(x)
+        if axis < 0:
+            axis += 1
     else:
-        cplxflag = False
-        if x.size(-1) != 2:
-            x = th.stack((x, th.zeros_like(x)), dim=-1)
+        realflag = False
 
     d = x.size(axis)
-    if nfft is None:
-        nfft = d
-    if d < nfft:
-        pad = list(x.size())
-        pad[axis] = nfft - d
-        z = th.zeros(pad, dtype=x.dtype, device=x.device)
-        x = th.cat((x, z), dim=axis)
-    elif d > nfft:
+    if (n is not None) and (d < n):
+        x = padfft(x, n, axis, shift)
+    elif d > n:
         raise ValueError('nfft is small than signal dimension!')
 
-    y = th.fft(x.transpose(axis, -2), signal_ndim=1, normalized=norm).transpose(axis, -2)
+    if shift:
+        y = thfft.fftshift(thfft.fft(thfft.fftshift(x, dim=axis), n=n, dim=axis, norm=norm), dim=axis)
+    else:
+        y = thfft.fft(x, n=n, dim=axis, norm=norm)
 
-    if cplxflag:
-        y = th.view_as_complex(y)
+    if realflag:
+        y = th.view_as_real(y)
 
     return y
 
 
-def ifft(x, nfft=None, axis=0, norm=False, shift=False):
-    """IFFT in torchlib
+def ifft(x, n=None, axis=0, norm="backward", shift=False):
+    """IFFT in tsar
 
-    IFFT in torchlib, since ifft in torch only supports complex-complex transformation,
+    IFFT in tsar, since ifft in torch only supports complex-complex transformation,
     for real ifft, we insert imaginary part with zeros (torch.stack((x,torch.zeros_like(x), dim=-1))),
     also you can use torch's rifft.
 
@@ -328,12 +385,15 @@ def ifft(x, nfft=None, axis=0, norm=False, shift=False):
         both complex and real representation are supported. Since torch does not
         support complex array, when :attr:`x` is complex, we will change the representation
         in real formation(last dimension is 2, real, imag), after IFFT, it will be change back.
-    nfft : {integer}, optional
+    n : {integer}, optional
         number of ifft points (the default is None --> equals to signal dimension)
     axis : {number}, optional
         axis of ifft (the default is 0, which the first dimension)
     norm : {bool}, optional
-        normalization (the default is False)
+        Normalization mode. For the backward transform (ifft()), these correspond to:
+        - "forward" - no normalization
+        - "backward" - normalize by ``1/n`` (default)
+        - "ortho" - normalize by 1``/sqrt(n)`` (making the IFFT orthonormal)
     shift : {bool}, optional
         shift the zero frequency to center (the default is False)
 
@@ -348,46 +408,45 @@ def ifft(x, nfft=None, axis=0, norm=False, shift=False):
         nfft is small than signal dimension.
     """
 
-    if th.is_complex(x):
-        cplxflag = True
-        x = th.view_as_real(x)
+    if norm is None:
+        norm = 'backward'
+
+    if (x.size(-1) == 2) and (not th.is_complex(x)):
+        realflag = True
+        x = th.view_as_complex(x)
+        if axis < 0:
+            axis += 1
     else:
-        cplxflag = False
-        if x.size(-1) != 2:
-            x = th.stack((x, th.zeros_like(x)), dim=-1)
+        realflag = False
 
-    d = x.size(axis)
-    if nfft is None:
-        nfft = d
-    if d < nfft:
-        pad = list(x.size())
-        pad[axis] = nfft - d
-        z = th.zeros(pad, dtype=x.dtype, device=x.device)
-        x = th.cat((x, z), dim=axis)
-    elif d > nfft:
-        raise ValueError('nfft is small than signal dimension!')
+    if shift:
+        y = thfft.ifftshift(thfft.ifft(thfft.ifftshift(x, dim=axis), n=n, dim=axis, norm=norm), dim=axis)
+    else:
+        y = thfft.ifft(x, n=n, dim=axis, norm=norm)
 
-    y = th.ifft(x.transpose(axis, -2), signal_ndim=1, normalized=norm).transpose(axis, -2)
+    if realflag:
+        y = th.view_as_real(y)
 
-    if cplxflag:
-        y = th.view_as_complex(y)
     return y
 
 
 if __name__ == '__main__':
 
-    import numpy as np
-
-    nfft = 8
+    print(th.__version__)
+    nfft = 4
+    ftshift = False
     x1 = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
     print(x1.shape)
     y1 = np.fft.fft(x1, n=nfft, axis=1, norm=None)
     print(y1, y1.shape)
+    x1 = np.fft.ifft(y1, n=nfft, axis=1, norm=None)
+    print(x1)
 
     x2 = th.tensor(x1, dtype=th.float32)
     x2 = th.stack([x2, th.zeros(x2.size())], dim=-1)
 
-    y2 = fft(x2, nfft=nfft, axis=1, norm=False)
+    y2 = fft(x2, n=nfft, axis=1, norm=None, shift=ftshift)
     print(y2, y2.shape)
-    x2 = ifft(y2, nfft=nfft, axis=1, norm=False)
+    x2 = ifft(y2, n=nfft, axis=1, norm=None, shift=ftshift)
     print(x2)
+
